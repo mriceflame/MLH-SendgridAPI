b5618e5cb33b85021636010e84cbd14655e423a8
raja-singh007 <raja.singh0011@gmail.com>
2021-01-14 05:17:55 +0530

    api has been setup with api key

---
+++ index.js
@@ -0,0 +1,22 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const sgMail = require('@sendgrid/mail')
+
+sgMail.setApiKey(process.env.API_KEY)
+
+const msg = {
+  to: 'test@example.com', // Change to your recipient
+  from: 'test@example.com', // Change to your verified sender
+  subject: 'MLH - Local Hack Day 4',
+  text: 'Fun to Hack with MLH',
+  html: '<strong>Learn and Fun with MLH daily hacks/strong>',
+}
+
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
